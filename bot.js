56568cbabd476295f780970fc95257ba109d7cc5
Samuel Pak <spak@dev.tempo>
2017-01-06 14:46:21 -0500

    Added fliptable. Search under construction. Promises are dumb.

--- bot.js
+++ bot.js
@@ -14,8 +14,11 @@ function output (err) {
 	}
 }
 
+var fetch_time;
+var messages;
 k2.on("ready", function (rawEvent) {
 	k2.user.setPresence({ game : { name : '!k2help' }});
+    fetch_time = 0;
 });
 
 //In this function we're going to add our commands.
@@ -23,25 +26,57 @@ k2.on("message", function (message) {
     if (message.content.substring(0, 1) == "!") {
         var arguments = message.content.substring(1).split(" ");
         var command = arguments[0];
-        arguments = arguments.splice(1);
+        //arguments = arguments.splice(1);
+        arguments.shift(); // no longer C-style argv
 
-			switch(command) {
-				case "hello" :
-					message.channel.sendMessage("The captain says you're a friend. I will not kill you.");
-					break;
-				case "goodbye" :
-					message.channel.sendMessage("Good riddance.");
-					break;
-				case "hug" :
-					message.channel.sendMessage("I'm not giving you a hug.");
-					break;
-				case "k2help" :
-					message.channel.sendMessage("You know, I'm not that complicated. You can just figure it out.");
-					break;
-				default :
-					break;		
-			} // switch
+        switch(command) {
+            case "hello" :
+                message.channel.sendMessage("The captain says you're a" +
+                    " friend. I will not kill you.");
+                break;
+            case "goodbye" :
+                message.channel.sendMessage("Good riddance.");
+                break;
+            case "hug" :
+                message.channel.sendMessage("I'm not giving you a hug.");
+                break;
+            case "k2help" :
+                message.channel.sendMessage("You know, I'm not that " +
+                    "complicated. You can just figure it out.");
+                break;
+            case "fliptable" :
+                message.channel.sendMessage("(╯°□°）╯︵ ┻━┻");
+                break;
+            /*case "search" :
+                if (arguments.size == null) {
+                    message.channel.sendMessage("Search for what, " 
+                        + message.author.username + "?");
+                }
+                
+                // fetch logic
+                if (fetch_time != 0) {
+                    if (Math.floor(Date.now()/1000) - fetch_time > 60) {
+                        messages = message.channel.fetchMessages(5000).prototype();
+                    }
+                    message.channel.sendMessage("Skipping fetch");  
+                }     
+                else { 
+                    fetch_time = Math.floor(Date.now()/1000);
+                    messages = message.channel.fetchMessage(5000);
+                    messages.then(function(val) { console.log("Promise fulfilled");});   
+                }
+                console.log("Type: " + typeof(messages));
+                // search logic
+                var results = messages.filter(function(keyval) {
+                    return keyval.content.includes(arguments[0]);
+                });
 
+                message.channel.sendMessage("**NOTE** : This command is " + 
+                    "still under construction.");
+                break; //case search
+            */
+            default :
+                break;		
+        } // switch
     } // if 
-
 });
