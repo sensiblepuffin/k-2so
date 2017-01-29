67a37fbb7368ad457fb02c8da7e7b6a320662cab
Samuel Pak <s.pak93@gmail.com>
2017-01-29 16:49:44 -0500

    Text responses are now accessed via a dictionary

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 K-2SO, a Discord bot.
+
+Use npm start to launch the bot. 
+
+Create a file 'token' that contains your Discord bot token. (Trailing newlines will cause Discord to choke, so get rid of those.) 
--- bot.js
+++ bot.js
@@ -1,9 +1,13 @@
 var Discord = require('discord.js');
 
-const token = 'MjY0NDUwNjE1MTM5MjM3ODk5.C0gwaA.KJmJVH4aWm1b2WAbbYjQLf6w7VA';
-
 var k2 = new Discord.Client();
 
+var fs = require ('fs');
+var path = require ('path');
+
+//require('token');
+
+var token = fs.readFileSync('token', 'utf8').replace(/^\s+|\s+$/g,'');
 k2.login(token);
 
 var fetch_time;
@@ -14,43 +18,29 @@ k2.on("ready", function (rawEvent) {
     fetch_time = 0;
 });
 
-//In this function we're going to add our commands.
+var responseDict = {
+	"hello": "The captain says you're a friend. I will not kill you.",
+	"goodbye" : "Good riddance.",
+	"hug" : "I'm not giving you a hug.",
+	"k2help" : "You know, I'm not that complicated. You can just figure it out.",
+	"fliptable" : "((╯°□°)╯︵ ┻━┻)"
+}
+
 k2.on("message", function (message) {
     if (message.content.substring(0, 1) == "!") {
         var arguments = message.content.substring(1).split(" ");
         var command = arguments[0];
-        //arguments = arguments.splice(1);
-        arguments.shift(); // no longer C-style argv
+		var channel = message.channel;
+        arguments.shift(); // just the arguments
 
-		
-
-        switch(command) {
-            case "hello" :
-                message.channel.sendMessage("The captain says you're a" +
-                    " friend. I will not kill you.");
-                break;
-            case "goodbye" :
-                message.channel.sendMessage("Good riddance.");
-                break;
-            case "hug" :
-                message.channel.sendMessage("I'm not giving you a hug.");
-                break;
-            case "k2help" :
-                message.channel.sendMessage("You know, I'm not that " +
-                    "complicated. You can just figure it out.");
-                break;
-            case "fliptable" :
-                message.channel.sendMessage("(╯°□°）╯︵ ┻━┻");
-                break;
-            default :
-                break;		
-        } // switch
+        if (responseDict[command]) { 
+                channel.sendMessage(responseDict[command]);
+        } // if
     } // if 
 	else {
 	 	if (message.toString().indexOf("JOHN CENA") !== -1) {
-			//message.react({ "id" : 272792565449752576, "name" : ":trumpet:"});
 			message.react("\uD83D\uDCA5"); 	// boom
 			message.react("\uD83C\uDFBA"); 	// trumpet
-		}
-	} // els
+		} // if
+	} // else
 });
